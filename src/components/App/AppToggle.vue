afcba51314da9c5dc790de2ef806b566c1d214b8
KreskoLab <redeezol@gmail.com>
2022-06-13 23:55:21 +0300

    feat: add AppToggle component

--- src/components/App/AppToggle.vue
+++ src/components/App/AppToggle.vue
@@ -1,11 +1,11 @@
 <template>
 	<div
 		class="inline-block relative rounded-full w-12 h-6 transition-colors duration-300 delay-75 ease cursor-pointer"
-		:class="background"
-		@click="toggle()"
+		:class="[background, { '!cursor-not-allowed': disabled }]"
+		@click="!disabled ? toggle() : ''"
 	>
 		<span
-			class="absolute inset-y-1 left-1 rounded-full dark:bg-light-100 w-4 h-4 transform duration-300 ease"
+			class="absolute inset-y-1 left-1 rounded-full bg-light-100 w-4 h-4 transform duration-300 ease"
 			:class="transition"
 		/>
 	</div>
@@ -15,23 +15,39 @@
 	setup
 	lang="ts"
 >
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 
 const props = defineProps<{
-	modelValue?: boolean
+	modelValue: boolean | string | number
+	trueValue?: string
+	falseValue?: string
+	disabled?: boolean
 }>()
 
 const emit = defineEmits<{
-	(event: 'input', value: boolean): void
+	(event: 'input', value: boolean | string | number): void
 }>()
 
-const state = ref<boolean>(props.modelValue || false)
+const computedTrue = computed(() => (props.trueValue ? props.trueValue : true))
+const computedFalse = computed(() => (props.falseValue ? props.falseValue : false))
 
-const transition = computed(() => (state.value ? 'translate-x-6' : 'translate-x-0'))
-const background = computed(() => (state.value ? 'dark:bg-purple-800' : 'dark:bg-dark-400'))
+const state = computed({
+	get() {
+		return props.modelValue || computedFalse.value
+	},
+
+	set(val) {
+		emit('input', val)
+	},
+})
+
+const transition = computed(() => (state.value === computedTrue.value ? 'translate-x-6' : 'translate-x-0'))
+const background = computed(() =>
+	state.value === computedTrue.value ? 'dark:bg-purple-800 bg-purple-600' : 'bg-dark-400'
+)
 
 function toggle() {
-	state.value = !state.value
-	emit('input', state.value)
+	if (state.value === computedTrue.value) state.value = computedFalse.value
+	else state.value = computedTrue.value
 }
 </script>
