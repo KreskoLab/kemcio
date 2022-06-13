e380cbeaefe7da60f3ac76eb0aa36ca3a983cb2b
KreskoLab <redeezol@gmail.com>
2022-06-13 23:55:37 +0300

    feat: add AppRange component

---
+++ src/components/App/AppRange.vue
@@ -0,0 +1,27 @@
+<script
+	setup
+	lang="ts"
+>
+defineProps<{
+	modelValue: number
+	disabled: boolean
+}>()
+
+defineEmits<{
+	(event: 'input', value: number): void
+}>()
+</script>
+
+<template>
+	<div>
+		<input
+			type="range"
+			min="0"
+			max="100"
+			:value="modelValue"
+			:disabled="disabled"
+			:class="{ 'cursor-not-allowed': disabled }"
+			@change="$emit('input', Number(($event.target as HTMLInputElement).value))"
+		/>
+	</div>
+</template>
