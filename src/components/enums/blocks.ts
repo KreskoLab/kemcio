5f25e3738e964bf9063c850cc9716003e94a2981
KreskoLab <redeezol@gmail.com>
2022-06-16 00:14:04 +0300

    feat: add icons enum

---
+++ src/components/enums/blocks.ts
@@ -0,0 +1,14 @@
+export enum BLOCKS {
+	TRIGGER = 'trigger',
+	IF = 'if',
+	DELAY = 'delay',
+	VARIABLE = 'variable',
+	EQUEL = 'equal',
+	GT = 'gt',
+	LT = 'lt',
+	POWER = 'power',
+	TEMPERATURE = 'temperature',
+	HUMIDITY = 'humidity',
+	Dimmer = 'dimmer',
+	Color = 'color',
+}
---
+++ src/components/enums/icons.ts
@@ -0,0 +1,7 @@
+export enum ICONS {
+	POWER = 'settings-2',
+	TEMPERATURE = 'thermometer-sun',
+	HUMIDITY = 'droplet',
+	Dimmer = 'sun',
+	Color = 'palette',
+}
