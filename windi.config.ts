f736428ef4f06d119bb4a8c1e6c5f9300f994f07
KreskoLab <redeezol@gmail.com>
2022-06-18 23:17:20 +0300

    chore: update windi config

--- windi.config.ts
+++ windi.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 	shortcuts: {
 		'page': 'dark:bg-dark-800 bg-purple-50/20',
 		'title': 'font-medium text-slate-700 dark:text-gray-200',
+		'subtitle': 'text-slate-900 dark:text-gray-400',
 		'form': 'dark:bg-dark-400 bg-slate-50 border border-gray-100 dark:border-dark-200 rounded-xl'
 	},
 	plugins: [require('@windicss/plugin-animations')()],
