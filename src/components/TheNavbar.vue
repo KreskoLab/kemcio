5e53ed869154dd831f1e388eee4de04863c3784a
KreskoLab <redeezol@gmail.com>
2022-06-24 22:43:43 +0300

    feat: add Navbar component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+	<header class="page fixed top-0 flex justify-between items-center py-4 px-6 h-16 w-full lg:hidden">
+		<router-link
+			class=""
+			to="/"
+		>
+			<img
+				src="@/assets/logo.svg"
+				class="w-8 h-8"
+			/>
+		</router-link>
+
+		<AppColorToggle />
+	</header>
+</template>
+
+<style scoped>
+header {
+	@apply dark:bg-dark-800/90 bg-white/90
+  border-b dark:border-dark-200 border-gray-200 z-50
+  backdrop-filter backdrop-blur;
+}
+</style>
