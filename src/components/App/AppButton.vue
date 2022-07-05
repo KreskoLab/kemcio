008470f72e3254182b1265726db4a7cd6320d3f8
KreskoLab <redeezol@gmail.com>
2022-07-06 01:21:52 +0300

    style (AppButton): add light theme for transparent variant

--- src/components/App/AppButton.vue
+++ src/components/App/AppButton.vue
@@ -1,26 +1,4 @@
-<template>
-	<button
-		class="w-full rounded-md text-light-50 transition duration-300"
-		:class="[
-			{
-				'px-2 py-1': $slots.default && size === 'sm',
-				'px-3 py-2': $slots.default && size === 'base',
-				'px-6 py-4': $slots.default && size === 'lg',
-				'bg-purple-500 ring-purple-400 dark:ring-offset-dark-600 hover:(bg-purple-500/90) focus:(ring-2 ring-offset-2 outline-none)':
-					!disabled && !transparent,
-				'cursor-not-allowed': disabled,
-			},
-		]"
-		:disabled="disabled"
-	>
-		<slot />
-	</button>
-</template>
-
-<script
-	setup
-	lang="ts"
->
+<script setup lang="ts">
 defineProps({
 	size: {
 		type: String,
@@ -41,3 +19,23 @@ defineProps({
 <script lang="ts">
 const sizes = ['sm', 'base', 'lg']
 </script>
+
+<template>
+	<button
+		class="w-full rounded-md text-light-50 transition duration-300"
+		:class="[
+			{
+				'px-2 py-1': $slots.default && size === 'sm',
+				'px-3 py-2': $slots.default && size === 'base',
+				'px-6 py-4': $slots.default && size === 'lg',
+				'bg-purple-500 ring-purple-400 dark:ring-offset-dark-600 hover:(bg-purple-500/90) focus:(ring-2 ring-offset-2 outline-none)':
+					!disabled && !transparent,
+				'cursor-not-allowed': disabled,
+				'text-slate-700 dark:text-light-100': transparent,
+			},
+		]"
+		:disabled="disabled"
+	>
+		<slot />
+	</button>
+</template>
