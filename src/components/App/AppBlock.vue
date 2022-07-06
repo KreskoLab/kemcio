ca93a7a6eb4740d3615639588a3313753580255f
KreskoLab <redeezol@gmail.com>
2022-07-07 01:15:42 +0300

    feat: add AppBlock component

---
+++ src/components/App/AppBlock.vue
@@ -0,0 +1,25 @@
+<script
+	setup
+	lang="ts"
+>
+import { useIcon } from '@/composables/get-icon'
+
+defineProps<{
+	title: string
+	icon: string
+}>()
+</script>
+
+<template>
+	<div
+		class="flex flex-col space-y-2 rounded-lg dark:bg-dark-300 bg-gray-100 dark:text-light-400 py-3 px-4 cursor-pointer overflow-hidden"
+	>
+		<svg
+			viewBox="0 0 24 24"
+			class="w-6 h-6"
+			v-html="useIcon(icon)"
+		/>
+
+		<span class="truncate">{{ title }}</span>
+	</div>
+</template>
