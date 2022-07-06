9829bd8e2ec0bb5af31e8cd956b501a088f768e5
KreskoLab <redeezol@gmail.com>
2022-07-07 01:15:11 +0300

    feat: add AppInfo component

---
+++ src/components/App/AppInfo.vue
@@ -0,0 +1,35 @@
+<script
+	setup
+	lang="ts"
+>
+defineProps<{
+	value: string | number
+}>()
+</script>
+
+<template>
+	<Transition
+		name="fade"
+		mode="out-in"
+		appear
+	>
+		<p
+			:key="value"
+			class="text-light-400 text-lg font-medium"
+		>
+			{{ value }}
+		</p>
+	</Transition>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+</style>
