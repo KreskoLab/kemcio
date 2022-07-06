573ce2ee17b2a957fba4843e3cb88dd7bbd98aec
KreskoLab <redeezol@gmail.com>
2022-07-07 01:20:09 +0300

    style (AppInput): classes clean up

--- src/components/App/AppInput.vue
+++ src/components/App/AppInput.vue
@@ -1,15 +1,20 @@
 <template>
 	<div class="flex flex-col space-y-2">
-		<label class="text-lg dark:text-gray-300">{{ label }}</label>
+		<label
+			v-if="label"
+			class="text-lg dark:text-gray-300"
+		>
+			{{ label }}
+		</label>
 
 		<div class="relative">
-			<div class="absolute inset-y-3 left-3 text-gray-500">
+			<div class="absolute inset-y-3 left-3 text-gray-600">
 				<slot name="icon" />
 			</div>
 
 			<div
 				v-if="password"
-				class="absolute inset-y-3 right-3 text-gray-500 cursor-pointer"
+				class="absolute inset-y-3 right-3 dark:text-gray-400 text-gray-400 cursor-pointer"
 				@click="showPassword()"
 			>
 				<i-lucide-eye v-if="inputType === 'password'" />
@@ -17,7 +22,7 @@
 			</div>
 
 			<input
-				class="w-full bg-white dark:bg-dark-400 text-slate-700 dark:text-gray-200 border-3 border-gray-200 dark:border-dark-200 transition duration-150 focus:(outline-none border-purple-300 dark:border-purple-300) py-2 pl-3 text-sm rounded-md"
+				class="w-full bg-white dark:bg-dark-400 text-gray-600 dark:text-gray-200 border-3 border-gray-200 dark:border-dark-200 transition duration-150 focus:(outline-none border-purple-300 dark:border-purple-300) py-2 pl-3 text-sm rounded-md"
 				:class="[
 					{
 						'border-red-400': error,
@@ -57,7 +62,7 @@
 import { ref, Ref } from 'vue'
 
 const props = defineProps<{
-	label: string
+	label?: string
 	placeholder?: string
 	modelValue?: string
 	error?: boolean
