821cce7378ea5e3fa704d34d2e20e35af6fed3d1
KreskoLab <redeezol@gmail.com>
2022-07-07 01:07:49 +0300

    feat: add enums

---
+++ src/enums/nodes.ts
@@ -0,0 +1,14 @@
+export enum NODES {
+	TRIGGER = 'NodeTrigger',
+	IF = 'NodeIf',
+	DELAY = 'NodeDelay',
+	VARIABLE = 'NodeVariable',
+	EQUEL = 'NodeEqual',
+	GT = 'NodeGt',
+	LT = 'NodeLt',
+	POWER = 'NodePower',
+	TEMPERATURE = 'NodeData',
+	HUMIDITY = 'NodeData',
+	Dimmer = 'dimmer',
+	Color = 'color',
+}
