821c3e2a65c7c221646776f25b54c567d4e9be11
KreskoLab <redeezol@gmail.com>
2022-07-07 22:48:10 +0300

    feat (workflow): add pause property

--- src/models/workflow.ts
+++ src/models/workflow.ts
@@ -3,6 +3,7 @@ import { GraphEdge, GraphNode } from '@braks/vue-flow'
 export type Workflow = {
 	_id: string
 	name: string
+	pause: boolean
 	nodes?: GraphNode[] | undefined
 	edges?: GraphEdge[] | undefined
 	createdAt: Date
