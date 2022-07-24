8dcca9f5322557dfc4d2fbae3892e17b3434250b
KreskoLab <redeezol@gmail.com>
2022-07-24 23:37:09 +0300

    style (App): add page transition

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,24 @@
 	<div class="flex flex-col-reverse lg:flex-row page">
 		<RouterView name="Sidenav" />
 
-		<main class="w-full h-screen lg:(min-h-screen)">
-			<Suspense>
-				<RouterView />
-			</Suspense>
+		<main
+			class="page w-full h-screen lg:(min-h-screen) scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-dark-300 scrollbar-track-transparent"
+		>
+			<RouterView v-slot="{ Component }">
+				<template v-if="Component">
+					<Transition name="fade">
+						<Suspense>
+							<component :is="Component" />
+
+							<template #fallback>
+								<div class="flex justify-center items-center padding w-full h-full">
+									<span class="animate-ping h-16 w-16 rounded-full bg-purple-600/60" />
+								</div>
+							</template>
+						</Suspense>
+					</Transition>
+				</template>
+			</RouterView>
 		</main>
 
 		<RouterView name="Navbar" />
@@ -14,4 +28,14 @@
 
 <style>
 @import '@/assets/main.css';
+
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
 </style>
