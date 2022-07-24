e8a22223aa8816c6d92b4c4afba302642b1058ab
KreskoLab <redeezol@gmail.com>
2022-07-24 21:55:41 +0300

    feat (workflows): wrap page in single root element

--- src/pages/workflows/index.vue
+++ src/pages/workflows/index.vue
@@ -28,7 +28,7 @@ const remove = reactive({
 	name: '',
 })
 
-onBeforeMount(async () => await getWorkflows())
+await getWorkflows()
 
 const onPause = (status: boolean) => (status ? 'Вимкнути' : 'Увімкнути')
 const isWokrking = (status: boolean) => (status ? 'Увімкнен' : 'Вимкнен')
@@ -87,124 +87,126 @@ async function getWorkflows() {
 </script>
 
 <template>
-	<AppModalDelete
-		v-if="remove.status"
-		title="Видалити сценарій"
-		@close="remove.status = false"
-		@remove="removeWorkflow(remove.id)"
-	>
-		<p class="subtitle text-base">Сценарій {{ remove.name }} буде видалено!</p>
-	</AppModalDelete>
+	<div>
+		<AppModalDelete
+			v-if="remove.status"
+			title="Видалити сценарій"
+			@close="remove.status = false"
+			@remove="removeWorkflow(remove.id)"
+		>
+			<p class="subtitle text-base">Сценарій {{ remove.name }} буде видалено!</p>
+		</AppModalDelete>
 
-	<AppModal
-		v-if="add"
-		size="base"
-		@close="add = false"
-	>
-		<template #header>
-			<h1 class="title text-lg">Сценарій</h1>
-		</template>
+		<AppModal
+			v-if="add"
+			size="base"
+			@close="add = false"
+		>
+			<template #header>
+				<h1 class="title text-lg">Сценарій</h1>
+			</template>
 
-		<template #body>
-			<div class="my-3">
-				<AppInput
-					v-model="name"
-					placeholder="Назва"
-				/>
-			</div>
-		</template>
+			<template #body>
+				<div class="my-3">
+					<AppInput
+						v-model="name"
+						placeholder="Назва"
+					/>
+				</div>
+			</template>
 
-		<template #footer>
-			<div class="flex space-x-2">
-				<AppButton
-					transparent
-					@click="resetNew()"
-				>
-					Назад
-				</AppButton>
-
-				<AppButton
-					v-if="rename.status"
-					@click="updateWorkflow(rename.id, { name: name })"
-				>
-					Оновити
-				</AppButton>
-
-				<AppButton
-					v-else
-					@click="addWorkflow()"
-				>
-					Додати
-				</AppButton>
-			</div>
-		</template>
-	</AppModal>
-
-	<div class="page flex flex-col pt-22 pb-16 px-6 lg:(pl-32 pt-8) h-full overflow-y-auto">
-		<div class="flex items-center space-x-5">
-			<h1 class="font-medium text-slate-700 dark:text-gray-200 text-3xl">Сценарії</h1>
-
-			<AppButton
-				size="sm"
-				class="w-max"
-				@click="add = true"
-			>
-				<span>новий</span>
-			</AppButton>
-		</div>
-
-		<div class="flex flex-col items-start gap-y-12 lg:(flex-row flex-wrap items-stretch gap-x-16 my-12) my-8">
-			<article
-				v-for="workflow in workflows"
-				:key="workflow._id"
-				class="flex flex-col space-y-4 form border-2 w-full lg:w-68 py-3 px-4"
-				:class="{
-					'hover:(dark:border-purple-400 border-purple-300 cursor-pointer transition duration-150)': !dropdownActive,
-				}"
-			>
-				<section class="flex justify-between items-center">
-					<router-link
-						:to="`/workflows/${workflow._id}`"
-						class="w-full"
+			<template #footer>
+				<div class="flex space-x-2">
+					<AppButton
+						transparent
+						@click="resetNew()"
 					>
-						<div class="flex items-center space-x-3">
-							<div class="bg-gray-100 dark:bg-dark-200 rounded-md w-max p-2">
-								<i-lucide-flask-conical class="title w-6 h-6" />
+						Назад
+					</AppButton>
+
+					<AppButton
+						v-if="rename.status"
+						@click="updateWorkflow(rename.id, { name: name })"
+					>
+						Оновити
+					</AppButton>
+
+					<AppButton
+						v-else
+						@click="addWorkflow()"
+					>
+						Додати
+					</AppButton>
+				</div>
+			</template>
+		</AppModal>
+
+		<div class="page flex flex-col pt-22 pb-16 px-6 lg:(pl-32 pt-8) h-full overflow-y-auto">
+			<div class="flex items-center space-x-5">
+				<h1 class="font-medium text-slate-700 dark:text-gray-200 text-3xl">Сценарії</h1>
+
+				<AppButton
+					size="sm"
+					class="w-max"
+					@click="add = true"
+				>
+					<span>новий</span>
+				</AppButton>
+			</div>
+
+			<div class="flex flex-col items-start gap-y-12 lg:(flex-row flex-wrap items-stretch gap-x-16 my-12) my-8">
+				<article
+					v-for="workflow in workflows"
+					:key="workflow._id"
+					class="flex flex-col space-y-4 form border-2 w-full lg:w-68 py-3 px-4"
+					:class="{
+						'hover:(dark:border-purple-400 border-purple-300 cursor-pointer transition duration-150)': !dropdownActive,
+					}"
+				>
+					<section class="flex justify-between items-center">
+						<router-link
+							:to="`/workflows/${workflow._id}`"
+							class="w-full"
+						>
+							<div class="flex items-center space-x-3">
+								<div class="bg-gray-100 dark:bg-dark-200 rounded-md w-max p-2">
+									<i-lucide-flask-conical class="title w-6 h-6" />
+								</div>
+
+								<h2 class="title truncate">{{ workflow.name }}</h2>
 							</div>
+						</router-link>
 
-							<h2 class="title truncate">{{ workflow.name }}</h2>
-						</div>
-					</router-link>
+						<AppDropdown @change="dropdownActive = $event">
+							<template #header>
+								<div class="title">
+									<i-lucide-more-horizontal />
+								</div>
+							</template>
 
-					<AppDropdown @change="dropdownActive = $event">
-						<template #header>
-							<div class="title">
-								<i-lucide-more-horizontal />
-							</div>
-						</template>
+							<AppDropdownItem @click="updateWorkflow(workflow._id, { pause: !workflow.pause })">
+								<i-lucide-toggle-right class="text-lg" />
+								<span>{{ onPause(workflow.pause) }}</span>
+							</AppDropdownItem>
 
-						<AppDropdownItem @click="updateWorkflow(workflow._id, { pause: !workflow.pause })">
-							<i-lucide-toggle-right class="text-lg" />
-							<span>{{ onPause(workflow.pause) }}</span>
-						</AppDropdownItem>
+							<AppDropdownItem @click="renameWorkflow(workflow.name, workflow._id)">
+								<i-lucide-pencil class="text-lg" />
+								<span>Редагувати</span>
+							</AppDropdownItem>
 
-						<AppDropdownItem @click="renameWorkflow(workflow.name, workflow._id)">
-							<i-lucide-pencil class="text-lg" />
-							<span>Редагувати</span>
-						</AppDropdownItem>
+							<AppDropdownItem @click="Object.assign(remove, { status: true, id: workflow._id, name: workflow.name })">
+								<i-lucide-trash class="text-lg" />
+								<span>Видалити</span>
+							</AppDropdownItem>
+						</AppDropdown>
+					</section>
 
-						<AppDropdownItem @click="Object.assign(remove, { status: true, id: workflow._id, name: workflow.name })">
-							<i-lucide-trash class="text-lg" />
-							<span>Видалити</span>
-						</AppDropdownItem>
-					</AppDropdown>
-				</section>
-
-				<section class="subtitle">
-					<h3 class="text-base">{{ isWokrking(workflow.pause) }}</h3>
-					<h3>{{ getDate(workflow.createdAt) }}</h3>
-				</section>
-			</article>
+					<section class="subtitle">
+						<h3 class="text-base">{{ isWokrking(workflow.pause) }}</h3>
+						<h3>{{ getDate(workflow.createdAt) }}</h3>
+					</section>
+				</article>
+			</div>
 		</div>
 	</div>
 </template>
