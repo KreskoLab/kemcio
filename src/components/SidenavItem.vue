e20ae1bfeb09e3fab288f06ee4b973390c65f910
KreskoLab <redeezol@gmail.com>
2022-07-25 13:44:21 +0300

    feat (SidenavItem): add active class

--- src/components/SidenavItem.vue
+++ src/components/SidenavItem.vue
@@ -1,10 +1,18 @@
-<script
-	setup
-	lang="ts"
->
-defineProps<{
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const props = defineProps<{
 	to?: string
 }>()
+
+const route = useRoute()
+
+const active = computed(() => {
+	if (route.matched.length > 1 && (props.to?.length as number) > 1) {
+		return route.path.includes(props.to as string) ? 'bg-light-300 dark:bg-dark-200' : ''
+	} else return route.path === props.to ? 'bg-light-300 dark:bg-dark-200' : ''
+})
 </script>
 
 <template>
@@ -12,6 +20,7 @@ defineProps<{
 		v-if="to"
 		:to="to"
 		class="link"
+		:class="active"
 	>
 		<slot />
 	</router-link>
@@ -19,6 +28,7 @@ defineProps<{
 	<div
 		v-else
 		class="link"
+		:class="active"
 	>
 		<slot />
 	</div>
