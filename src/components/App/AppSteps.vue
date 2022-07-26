518626ed9ef83ac01c2e7ab91f0686efe85bf203
KreskoLab <redeezol@gmail.com>
2022-07-26 22:50:54 +0300

    refactor (AppSteps): replace div with ul

--- src/components/App/AppSteps.vue
+++ src/components/App/AppSteps.vue
@@ -6,12 +6,12 @@ defineProps<{
 </script>
 
 <template>
-	<div class="flex space-x-6 justify-center">
-		<div
+	<ul class="flex space-x-6 justify-center">
+		<li
 			v-for="n in steps"
 			:key="n"
 			class="h-2 w-1/3 rounded-full border-2 border-purple-500"
 			:class="{ 'bg-purple-500': n < step + 1 }"
 		/>
-	</div>
+	</ul>
 </template>
